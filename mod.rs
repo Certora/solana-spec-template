247a1f8ac706b5d475755c798e9797fa3ac725df
Arie Gurfinkel <arie@certora.com>
2025-05-02 20:15:09 -0400

    initial import

---
+++ mod.rs
@@ -0,0 +1,12 @@
+#[cfg(feature = "certora")]
+pub mod nondet;
+
+#[cfg(feature = "certora")]
+pub mod log;
+
+#[cfg(feature = "certora")]
+pub mod mocks;
+
+#[cfg(feature = "certora")]
+pub mod specs;
+
---
+++ nondet.rs
@@ -0,0 +1,1 @@
+/// Implementations for cvlr::nondet::Nondet trait
